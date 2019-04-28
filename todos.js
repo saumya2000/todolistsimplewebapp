dd61842afa9d63cfc481e3fb30b5d6ff237bf17f
saumya2000 <36287195+saumya2000@users.noreply.github.com>
2019-04-28 21:18:26 +0530

    Add files via upload

---
+++ todos.js
@@ -0,0 +1,23 @@
+//check off specific todos by clicking
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+    	$(this).remove();
+    });
+	event.stopPropagation();
+});
+//enter keycode is 13
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		//grabbing new todo text from input
+		var todotext=$(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todotext + "</li>");
+	}
+	
+});
